467b1be74d9f9e017de0ba33b16c3163f10580a7
passionat <96873789+passionat@users.noreply.github.com>
2023-06-24 15:26:11 +0530

    Add files via upload

---
+++ user.sql
@@ -0,0 +1,13 @@
+use hello;
+-- create table user(
+-- staff_id int,
+-- room_id int,
+-- event_name varchar(255),
+-- target_audi varchar(255),
+-- num_attend int,
+-- constraint attend_const check(num_attend <= 500));
+
+
+-- alter table user add constraint user_staff_fk foreign key(staff_id) references staff(staff_id);
+-- alter table user add constraint user_room_fk foreign key(room_id) references room(room_id);
+
